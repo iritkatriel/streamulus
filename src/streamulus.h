027f794d176239eb53f0c3bc2b84326251e669a6
Irit Katriel <iritkatriel@yahoo.com>
2012-06-19 14:59:30 +0100

    merged src in include

---
+++ src/streamulus.h
@@ -0,0 +1,20 @@
+//
+//  streamulus.h
+//
+//  Copyright (c) 2012 Irit Katriel. All rights reserved.
+//
+
+#pragma once
+
+#include "engine.h"
+#include "input_stream.h"
+#include "operators.h"
+#include "streamify.h"
+#include "strop_const.h"
+#include "strop_data_source.h"
+
+
+namespace streamulus
+{
+    typedef Engine Streamulus;    
+} // ns streamulus
