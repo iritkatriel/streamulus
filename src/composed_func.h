13d3f5b941ce16896f1d343ae5e8e5690e74cfad
Irit Katriel <iritkatriel@yahoo.com>
2012-07-30 12:00:51 +0100

    first version - just unary function composition

---
+++ src/composed_func.h
@@ -0,0 +1,14 @@
+//
+//  compose_fn.h
+//  streamulus
+//
+//  Created by Irit Katriel on 21/07/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef streamulus_compose_fn_h
+#define streamulus_compose_fn_h
+
+
+
+#endif
