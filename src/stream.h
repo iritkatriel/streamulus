16df5478049e775f2edb42ba845bf3bb5683f44f
iritkatriel <iritkatriel@yahoo.com>
2016-01-01 17:12:02 +0000

    added boost tests for stream.h

--- src/stream.h
+++ src/stream.h
@@ -25,12 +25,19 @@
 #include <deque>
 #include <boost/optional.hpp>
 
-namespace streamulus
-{    
+namespace streamulus {
+
+    /**
+     * A stream of Ts, FIFO.
+     * Initially empty. Append adds data.
+     * HasMore() indicates whether there is unread data in the queue.
+     * IsValid() is true if there is data in the queue (read or unread).
+     * Current() returns the current value, which is the oldest unread data if there is any, or the youngest read data otherwise.
+     */
     template<typename T>
     class Stream : public StreamBase
     {
-    public:	
+    public:
 
         void Append(const T& item)
         {
@@ -41,12 +48,12 @@ namespace streamulus
         {
             return !mBuffer.empty();
         }
-        
+
         bool IsValid()
         {
             return mLastValue || !mBuffer.empty();
         }
-        
+
         const T& Current()
         {
             if (!mBuffer.empty())
@@ -55,16 +62,19 @@ namespace streamulus
                 mBuffer.pop_front();
             }
 
+            if (!mLastValue) {
+                throw std::invalid_argument("Current() called for an empty stream");
+            }
             assert(mLastValue);
-            
+
             // No new data - return last
             return *mLastValue;
         }
-                        
+
     private:
-                
+
         std::deque<T> mBuffer;
         boost::optional<T> mLastValue;
     };
-    
+
 } // ns streamulus
